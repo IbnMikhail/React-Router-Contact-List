9449215c0d62ecf4ea007ccbf11490a1284e83a6
IbnMikhail <mikhailtoby@gmail.com>
2023-08-31 16:56:51 +0100

    adding pathless parts

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,10 @@ import {
 import "./index.css";
 import ErrorPage from "./error-page";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
-import Contact, { loader as contactLoader, } from "./routes/contact";
+import Contact, { loader as contactLoader, action as contactAction, } from "./routes/contact";
 import EditContact, { action as editAction, } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 
 
@@ -22,9 +23,21 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+      { index: true, element: <Index /> },
+      { 
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
@@ -39,10 +52,8 @@ const router = createBrowserRouter([
       },
     ],
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
+]
+},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
